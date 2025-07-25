72dd8b107faa0fb8f05770b34db0a2804b1db183
Rakelilika <raqueldirey@gmail.com>
2025-07-26 01:26:18 +0200

    Creacion de proyecto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+          <h1 className="text-4xl font-bold text-blue-600">¡Tailwind funciona!</h1>
+        </div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
